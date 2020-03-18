97be466658d942f24fe2f72ce1d1bfa1c4065556
chuck richmond <chuck@firetoad.net>
2020-03-18 16:46:40 -0700

    added in file handling

--- ew.py
+++ ew.py
@@ -1,7 +1,7 @@
 import boto3
 import json
 import csv
-client = boto3.client('workspaces', region_name = 'us-west-2')
+client = boto3.client('workspaces', region_name = 'eu-central-1')
 paginator = client.get_paginator('describe_workspaces')
 
 # for page in page_iterator:
@@ -10,10 +10,26 @@ paginator = client.get_paginator('describe_workspaces')
 #         UserInfo = (workspaces["UserName"], client.describe_tags(ResourceId = workspaces["WorkspaceId"]))
 #         print (UserInfo)
 i = 0
-for workspace in [workspaces  for page in paginator.paginate() for workspaces in page["Workspaces"]]:
+with open ('temp.csv' , 'w') as csv_file:
+    fieldnames = ['UserName', 'Division', 'Contractor', 'Auto-Provisioned']
+    writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
+    writer.writeheader()
+    for workspace in [workspaces  for page in paginator.paginate() for workspaces in page["Workspaces"]]:
     # print(workspace)
-    i = i + 1
-    UserInfo = (workspace["UserName"], [tag['Value']  for tag in client.describe_tags(ResourceId = workspace["WorkspaceId"])["TagList"] if tag['Key'] in ['div', 'dept']])
-    print (UserInfo, i)
+        i = i + 1
+        tags = client.describe_tags(ResourceId = workspace["WorkspaceId"])["TagList"]
+        #print (tags)
+        division = [tag["Value"]  for tag in tags if tag['Key'] in ['div', 'Div']]
+        UserInfo = {
+         'UserName' : workspace["UserName"], \
+         'Division' :  division[0] if division else "na",\
+         'Contractor' : 'A-' in workspace['UserName'], 
+         'Auto-Provisioned' : ['True','true'] in [tag2['Value']  for tag2 in tags if tag2['Key'] in ['AutoProvision']],\
+         
+         }
+        print (UserInfo)
+        writer.writerow(UserInfo)
+    
+
     
     
