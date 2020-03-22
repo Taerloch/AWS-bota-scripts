49383e62aba346e832fdd5487037f93eea53c26a
chuck richmond <chuck.richmond@paccar.com>
2020-03-22 12:55:18 -0700

    added Last Login to the output json

--- ew.py
+++ ew.py
@@ -1,23 +1,22 @@
 import boto3
 import json
 import csv
-client = boto3.client('workspaces', region_name = 'eu-central-1')
+import datetime
+client = boto3.client('workspaces', region_name='us-west-2')
 paginator = client.get_paginator('describe_workspaces')
 
-# for page in page_iterator:
-# #    print (page["Workspaces"])
-#     for workspaces in page["Workspaces"]:
-#         UserInfo = (workspaces["UserName"], client.describe_tags(ResourceId = workspaces["WorkspaceId"]))
-#         print (UserInfo)
 i = 0
-with open ('temp.csv' , 'w') as csv_file:
-    fieldnames = ['UserName', 'Division', 'Contractor', 'Auto-Provisioned']
+with open (boto3.session.Session().region_name + '.csv' , 'w') as csv_file:
+    fieldnames = ['UserName', 'Division', 'Contractor', 'Auto-Provisioned', 'ComputerName', 'IpAddress', 'LastLogin']
     writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
     writer.writeheader()
     for workspace in [workspaces  for page in paginator.paginate() for workspaces in page["Workspaces"]]:
     # print(workspace)
         i = i + 1
         tags = client.describe_tags(ResourceId = workspace["WorkspaceId"])["TagList"]
+        status = client.describe_workspaces_connection_status(WorkspaceIds = [workspace["WorkspaceId"]])["WorkspacesConnectionStatus"][0].get('LastKnownUserConnectionTimestamp', datetime.date(2030, 1, 1))
+        # last_login = status['LastKnownUserConnectionTimestamp'] if status else "Never"
+        print (status)
         #print (tags)
         division = [tag["Value"]  for tag in tags if tag['Key'] in ['div', 'Div']]
         UserInfo = {
@@ -25,11 +24,14 @@ with open ('temp.csv' , 'w') as csv_file:
          'Division' :  division[0] if division else "na",\
          'Contractor' : 'A-' in workspace['UserName'], 
          'Auto-Provisioned' : ['True','true'] in [tag2['Value']  for tag2 in tags if tag2['Key'] in ['AutoProvision']],\
-         
+         'ComputerName' :  workspace["ComputerName"], \
+         'IpAddress' : workspace["IpAddress"], \
+         'LastLogin' : status
+
          }
         print (UserInfo)
         writer.writerow(UserInfo)
     
 
-    
+
     
