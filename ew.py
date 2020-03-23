59290665da219a7aa1f304acbc37e69db3ee1179
chuck richmond <chuck.richmond@paccar.com>
2020-03-22 18:43:52 -0700

    transitioning to writing to dynamodb

--- ew.py
+++ ew.py
@@ -2,36 +2,49 @@ import boto3
 import json
 import csv
 import datetime
-client = boto3.client('workspaces', region_name='us-west-2')
-paginator = client.get_paginator('describe_workspaces')
+from datetime import date
+def get_Workspaces(client, userInfos):
+    paginator = client.get_paginator('describe_workspaces')
 
-i = 0
-with open (boto3.session.Session().region_name + '.csv' , 'w') as csv_file:
-    fieldnames = ['UserName', 'Division', 'Contractor', 'Auto-Provisioned', 'ComputerName', 'IpAddress', 'LastLogin']
-    writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
-    writer.writeheader()
+    i = 0
+        
     for workspace in [workspaces  for page in paginator.paginate() for workspaces in page["Workspaces"]]:
-    # print(workspace)
+        # print(workspace)
         i = i + 1
         tags = client.describe_tags(ResourceId = workspace["WorkspaceId"])["TagList"]
-        status = client.describe_workspaces_connection_status(WorkspaceIds = [workspace["WorkspaceId"]])["WorkspacesConnectionStatus"][0].get('LastKnownUserConnectionTimestamp', datetime.date(2030, 1, 1))
-        # last_login = status['LastKnownUserConnectionTimestamp'] if status else "Never"
-        print (status)
-        #print (tags)
+        last_login = client.describe_workspaces_connection_status(WorkspaceIds = [workspace["WorkspaceId"]])["WorkspacesConnectionStatus"][0].get('LastKnownUserConnectionTimestamp', datetime.date(2030, 1, 1))
         division = [tag["Value"]  for tag in tags if tag['Key'] in ['div', 'Div']]
         UserInfo = {
-         'UserName' : workspace["UserName"], \
-         'Division' :  division[0] if division else "na",\
-         'Contractor' : 'A-' in workspace['UserName'], 
-         'Auto-Provisioned' : ['True','true'] in [tag2['Value']  for tag2 in tags if tag2['Key'] in ['AutoProvision']],\
-         'ComputerName' :  workspace["ComputerName"], \
-         'IpAddress' : workspace["IpAddress"], \
-         'LastLogin' : status
+        'WorkspaceId' :  workspace["WorkspaceId"],\
+        'UserName' : workspace["UserName"], \
+        'Division' :  division[0] if division else "na",\
+        'Contractor' : 'A-' in workspace['UserName'], 
+        'Auto-Provisioned' : ['True','true'] in [tag2['Value']  for tag2 in tags if tag2['Key'] in ['AutoProvision']],\
+        'ComputerName' :  workspace["ComputerName"], \
+        'IpAddress' : workspace["IpAddress"], \
+        'LastLogin' : last_login.strftime("%m/%d/%Y, %H:%M:%S")
 
-         }
+        }
         print (UserInfo)
-        writer.writerow(UserInfo)
-    
+        userInfos.append(UserInfo)
+        putinDyn(UserInfo)
+def putinDyn(UserInfo):
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
 
+    table = dynamodb.Table('Workspaces')
+    response = table.put_item( Item = UserInfo)
+
+          
+userInfos = []
+get_Workspaces(boto3.client('workspaces', region_name='us-west-2'), userInfos)
+get_Workspaces(boto3.client('workspaces', region_name='eu-central-1'), userInfos)
+get_Workspaces(boto3.client('workspaces', region_name='ap-southeast-2'), userInfos)
+
+filename  = "workspaces" + date.today().isoformat() + ".csv"
+with open (filename , 'w') as csv_file:
+    fieldnames = ['WorkspaceId','UserName', 'Division', 'Contractor', 'Auto-Provisioned', 'ComputerName', 'IpAddress', 'LastLogin']
+    writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
+    writer.writeheader()
+    writer.writerows(userInfos)
 
     
