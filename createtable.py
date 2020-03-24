d2c58574a858a64b71257fa6950dc2cd147ae4ed
chuck richmond <chuck.richmond@paccar.com>
2020-03-23 17:09:25 -0700

    added in correct formatting for downstream.

--- createtable.py
+++ createtable.py
@@ -14,7 +14,8 @@
 from __future__ import print_function # Python 2/3 compatibility
 import boto3
 
-dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
+#dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 
 
 table = dynamodb.create_table(
--- ew.py
+++ ew.py
@@ -2,35 +2,51 @@ import boto3
 import json
 import csv
 import datetime
+import sys
 from datetime import date
+debug = True
 def get_Workspaces(client, userInfos):
     paginator = client.get_paginator('describe_workspaces')
-
+    
     i = 0
         
     for workspace in [workspaces  for page in paginator.paginate() for workspaces in page["Workspaces"]]:
-        # print(workspace)
-        i = i + 1
+        
+        # PACCAR metadata is stored in tags, at this time I am only scraping division code
         tags = client.describe_tags(ResourceId = workspace["WorkspaceId"])["TagList"]
-        last_login = client.describe_workspaces_connection_status(WorkspaceIds = [workspace["WorkspaceId"]])["WorkspacesConnectionStatus"][0].get('LastKnownUserConnectionTimestamp', datetime.date(2030, 1, 1))
         division = [tag["Value"]  for tag in tags if tag['Key'] in ['div', 'Div']]
+        
+        # Need to prune workstation that have not been used.
+        try:
+            last_login = client.describe_workspaces_connection_status(WorkspaceIds = [workspace["WorkspaceId"]]).get("WorkspacesConnectionStatus",[{"LastKnownUserConnectionTimestamp" : "1/1/2020"}]).pop().get('LastKnownUserConnectionTimestamp', datetime.date(2030, 1, 1))
+        except:
+            last_login = datetime.date(2030, 1, 1)   
+        #TODO: add in the timestamp of observation. and the region and department
         UserInfo = {
-        'WorkspaceId' :  workspace["WorkspaceId"],\
-        'UserName' : workspace["UserName"], \
-        'Division' :  division[0] if division else "na",\
-        'Contractor' : 'A-' in workspace['UserName'], 
-        'Auto-Provisioned' : ['True','true'] in [tag2['Value']  for tag2 in tags if tag2['Key'] in ['AutoProvision']],\
-        'ComputerName' :  workspace["ComputerName"], \
-        'IpAddress' : workspace["IpAddress"], \
-        'LastLogin' : last_login.strftime("%m/%d/%Y, %H:%M:%S")
+            'WorkspaceId' :  workspace["WorkspaceId"],\
+            'UserName' : workspace["UserName"], \
+            'Division' :  division[0] if division else "na",\
+            'Contractor' : 'A-' in workspace['UserName'], 
+            'Auto-Provisioned' : ['True','true'] in [tag2['Value']  for tag2 in tags if tag2['Key'] in ['AutoProvision']],\
+            'ComputerName' :  workspace["ComputerName"], \
+            'IpAddress' : workspace["IpAddress"], \
+            'LastLogin' : last_login.strftime("%Y-%m-%d")
 
-        }
-        print (UserInfo)
-        userInfos.append(UserInfo)
+            }
+       
+        
+        if debug : 
+            
+            print(UserInfo, end = '\r', flush = True)
+       
+
+        # userInfos.append(UserInfo)
         putinDyn(UserInfo)
-def putinDyn(UserInfo):
-    dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
 
+# step to add the user information to the Workspaces Table
+# dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+def putinDyn(UserInfo):
     table = dynamodb.Table('Workspaces')
     response = table.put_item( Item = UserInfo)
 
